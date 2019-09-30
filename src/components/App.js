477c8796f96661c12914eebbb23576baf4698a90
Oleg Pavlyuk <pavlyuk.o.yu@gmail.com>
2019-09-30 20:41:04 +0300

    add bootstrap and styles for todo app

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,28 @@ import React from 'react';
 import AppHeader from './AppHeader';
 import SearchPanel from './SearchPanel';
 import TodoList from './TodoList';
+import ItemStatusFilter from './ItemStatusFilter'
+
+import './App.css';
 
 const App = () => {
   const todoData = [
-    { label: 'Drink coffee', important: false },
-    { label: 'Crete React App', important: true },
-    { label: 'Make a lunch', important: false }
-  ]
+    { label: 'Drink coffee', important: false, id: 1 },
+    { label: 'Crete React App', important: true, id: 2 },
+    { label: 'Make a lunch', important: false, id: 3 }
+  ];
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos={todoData}/>
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
