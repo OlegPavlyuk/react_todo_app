e207776716262838289aac00ae1b0ce3209e3003
Oleg Pavlyuk <pavlyuk.o.yu@gmail.com>
2019-10-01 10:56:14 +0300

    add ability to delete list items

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppHeader from '../AppHeader';
 import SearchPanel from '../SearchPanel';
 import TodoList from '../TodoList';
@@ -6,24 +6,43 @@ import ItemStatusFilter from '../ItemStatusFilter';
 
 import './App.css';
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink coffee', important: false, id: 1 },
-    { label: 'Crete React App', important: true, id: 2 },
-    { label: 'Make a lunch', important: false, id: 3 }
-  ];
+export default class App extends Component {
+  state = {
+    todoData: [
+      { label: 'Drink coffee', important: false, id: 1 },
+      { label: 'Crete React App', important: true, id: 2 },
+      { label: 'Make a lunch', important: false, id: 3 }
+    ]
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  deleteItem = id => {
+    this.setState(({ todoData }) => {
+      const index = todoData.findIndex(el => el.id === id);
+      const newArray = [
+        ...todoData.slice(0, index),
+        ...todoData.slice(index + 1)
+      ];
+
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList
+          todos={this.state.todoData}
+          onDeleted={id => this.deleteItem(id)}
+        />
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
